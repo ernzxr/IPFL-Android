aa9e17b2803a357be7c66bbd103ec932e2b1e3ce
ernzxr <103536591+ernzxr@users.noreply.github.com>
2023-11-27 17:03:19 -0300

    Connect Curso and MySQL

---
+++ Apps/CRUB/app/src/main/java/com/example/crub/Curso.java
@@ -0,0 +1,52 @@
+package com.example.crub;
+
+public class Curso {
+    private int idcurso, idinstituto;
+    private String horario, nivel;
+
+    public Curso() {
+    }
+
+    public Curso(int idcurso, int idinstituto, String horario, String nivel) {
+        this.idcurso = idcurso;
+        this.idinstituto = idinstituto;
+        this.horario = horario;
+        this.nivel = nivel;
+    }
+
+    public int getIdcurso() {
+        return idcurso;
+    }
+
+    public void setIdcurso(int idcurso) {
+        this.idcurso = idcurso;
+    }
+
+    public int getIdinstituto() {
+        return idinstituto;
+    }
+
+    public void setIdinstituto(int idinstituto) {
+        this.idinstituto = idinstituto;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public String toString() {
+        return nivel;
+    }
+}
---
+++ Apps/CRUB/app/src/main/java/com/example/crub/LeerCursos.java
@@ -0,0 +1,33 @@
+package com.example.crub;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class LeerCursos extends AsyncTask<Void, Void, ArrayList<Curso>> {
+
+    @Override
+    protected ArrayList<Curso> doInBackground(Void... voids) {
+        ResultSet result = null;
+        ArrayList<Curso> list = new ArrayList<>();
+        try {
+            Statement st = MySQL.conexion.createStatement();
+            String sql = "select * from curso";
+            result = st.executeQuery(sql);
+            while (result.next()) {
+                int idcurso = result.getInt("idcurso");
+                int idinstituto = result.getInt("instituto_idinstituto");
+                String horario = result.getString("horario");
+                String nivel = result.getString("nivel");
+                list.add(new Curso(idcurso, idinstituto, horario, nivel));
+            }
+        }
+        catch (Exception e) {
+            Log.e("Error en select", e.getMessage());
+        }
+        return list;
+    }
+}
