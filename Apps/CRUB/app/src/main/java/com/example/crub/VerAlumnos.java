9f41ceccd9a41e25e370f584447d7bfe73ef9a37
ernzxr <103536591+ernzxr@users.noreply.github.com>
2023-12-11 16:44:16 -0300

    Added VerAlumnos

--- Apps/CRUB/app/src/main/java/com/example/crub/VerAlumnos.java
+++ Apps/CRUB/app/src/main/java/com/example/crub/VerAlumnos.java
@@ -5,66 +5,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
 public class VerAlumnos extends AppCompatActivity {
     private ListView listViewAlumnos;
-    private ArrayList<Alumno> alumnosList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_alumno);
         listViewAlumnos = findViewById(R.id.listViewAlumnos);
         this.mostrarAlumnos();
-        this.cargar();
     }
 
-    private class MostrarAlumnos extends AsyncTask<Void, Void, String>{
+    private class MostrarAlumnos extends AsyncTask<Void, Void, ArrayList<String>>{
         @Override
-        protected String doInBackground(Void... voids) {
-            String mensaje = "";
+        protected ArrayList<String> doInBackground(Void... voids) {
+            ResultSet result = null;
+            ArrayList<String> alumnosList = new ArrayList<>();
             try {
                 // Crear sentencia para insertar
                 Statement st = MySQL.conexion.createStatement();
-                String sql = "select * from crub.alumno ";
-                st.executeUpdate(sql);
+                String sql = "select nombre, nivel from alumno join curso on curso_idcurso = idcurso order by nivel, nombre";
+                result = st.executeQuery(sql);
+                while (result.next()) {
+                    String nombre  = result.getString("nombre");
+                    String curso   = result.getString("nivel");
+                    alumnosList.add(curso +" - "+nombre);
+                }
             } catch (SQLException e) {
-                mensaje = "Error en la peticion " + e.getMessage() + " " + e.getErrorCode();
-            }
-            return mensaje;
+                Log.e("Error al leer anotados", e.getMessage() + " " + e.getErrorCode());            }
+            return alumnosList;
         }
     }
 
-    public void cargar(){
-        String mensaje = "";
-        try {
-            mensaje = new MostrarAlumnos().execute().get();
-        }
-        catch (Exception e) {
-            mensaje = e.getMessage();
-        }
-
-        Alumno alumno = (Alumno) listViewAlumnos.getSelectedItem();
-
-        if(mensaje.equals("")) {
-            Toast.makeText(this,"Datos cargados", Toast.LENGTH_LONG).show();
-        }
-        else {
-            Toast.makeText(this,"Error: "+mensaje, Toast.LENGTH_LONG).show();
-        }
-    }
     public void mostrarAlumnos(){
         try {
-            alumnosList = new LeerAlumnos().execute().get();
-            ArrayAdapter<Alumno> adapter = new ArrayAdapter<Alumno>(this, android.R.layout.simple_list_item_1, alumnosList);
-            this.listViewAlumnos.setAdapter(adapter);
+            ArrayList<String> alumnosList = new MostrarAlumnos().execute().get();
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alumnosList);
+            listViewAlumnos.setAdapter(adapter);
         }
         catch (Exception e) {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
